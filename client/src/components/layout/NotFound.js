990a0c3a01a0ded4c1216ff23330dfee4bc81157
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-06-20 20:31:23 +0530

    #Optimized Imports

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,48 +1,9 @@
 import React from 'react';
 // import '../404-star-wars-bb-8/js/404'
-import '../404-star-wars-bb-8/css/404.css'
-import { Link } from 'react-router-dom'
 
 export default () => {
   return (
-    <body >
-
-    <main style={{ background: '#869F9D',overflow: 'hidden',width: '100%',minHeight: '100%',color: '#FFF'}}>
-      <div className='sky' id="message" style={{minHeight:'100%', width: '100%',height: '100%',color: '#FFF',zIndex:'11',
-        background: '#869F9D'}}>
-        <h3>ERROR</h3>
-        <h1>404</h1>
-        <p id="quote">"Why Does Everyone Want To <Link to="/dashboard">Go Back</Link> To Jakku?!"</p>
-      </div>
-    {/*</div>*/}
-
-      <div className="sand"/>
-      <div className="bb8">
-        <div className="antennas">
-          <div className="antenna short"></div>
-          <div className="antenna long"></div>
-        </div>
-        <div className="head">
-          <div className="stripe one"></div>
-          <div className="stripe two"></div>
-          <div className="eyes">
-            <div className="eye one"></div>
-            <div className="eye two"></div>
-          </div>
-          <div className="stripe three"></div>
-        </div>
-        <div className="ball">
-          <div className="lines one"></div>
-          <div className="lines two"></div>
-          <div className="ring one"></div>
-          <div className="ring two"></div>
-          <div className="ring three"></div>
-        </div>
-        <div className="shadow"></div>
-      </div>
-    </main>
-
-    </body>
+    <h1>Page not found</h1>
   );
 };
 
