0e81b4bd2e76c55babaf7f855ef6867fc06e3c77
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-06-20 19:33:37 +0530

    #Added ProcFile #Fixed a minor bug

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api/diagAdmin',diagAdmin)
 if(process.env.NODE_ENV === 'production') {
   //Set static folder
   console.log({'IN node_env':process.env.NODE_ENV})
-  app.use(app.static('client/build'));
+  app.use(express.static('client/build'));
   app.get('*',(req,res) => {
     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   })
