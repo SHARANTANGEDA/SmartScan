5e3c94daf214301a20d94772db7ff2f320573ac7
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-06-21 12:47:25 +0530

    #ALPHA TEST RELEASE

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default () => {
   return (
-    <footer className=" text-white mt-5 p-4 text-center " style={{background: 'black', height:'60px'
-      ,position:'absolute',
-      left:0,
-      bottom:0,
-      right:0}}>
-      Copyright &copy; {new Date().getFullYear()} L V Prasad Eye Institute
-    </footer>
+
+      <footer className=" text-white mt-5 p-4 text-center " style={{background: 'black', height:'60px',left:0,
+        bottom:0,
+        right:0}}>
+        Copyright &copy; {new Date().getFullYear()} L V Prasad Eye Institute
+      </footer>
+
   );
 };
