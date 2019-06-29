9ad08db1f34fab3d361d9e880ca6caa708813c40
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-06-29 15:49:18 +0530

    #Added LVPEI API

--- routes/api/diagAdmin.js
+++ routes/api/diagAdmin.js
@@ -11,37 +11,39 @@ const Diagnostics=require('../../mongoModels/Diagnostics')
 const Patient = require('../../mongoModels/Patient')
 const uploadFilesInput = require('../../validations/uploadFiles')
 const db = require('../../models')
+const request = require('request')
+const API_KEY = require('../../config/keys').mongoURI;
 
-// router.post('/registerSA',(req, res) => {
-//   // const { errors, isValid } = validateRegisterInput(req.body)
-//   // if (!isValid) {
-//   //   return res.status(400).json(errors)
-//   // }
-//   User.findOne({ emailId: req.body.emailId }).then(user => {
-//
-//     if (user) {
-//       return res.status(400).json('errors')
-//     } else {
-//       const newUser = new User({
-//         emailId: req.body.emailId,
-//         password: req.body.password,
-//         role: 'super_admin'
-//       })
-//       bcrypt.genSalt(10, (err, salt) => {
-//         bcrypt.hash(newUser.password, salt, (err, hash) => {
-//           if (err) throw err
-//           newUser.password = hash
-//           newUser.save().then(user => {
-//             res.json({success: true,user})
-//           }).catch(err => {
-//             console.log(err)
-//             res.json({error: 'creating the user'})
-//           })
-//         })
-//       })
-//     }
-//   })
-// })
+router.post('/registerSA',(req, res) => {
+  // const { errors, isValid } = validateRegisterInput(req.body)
+  // if (!isValid) {
+  //   return res.status(400).json(errors)
+  // }
+  User.findOne({ emailId: req.body.emailId }).then(user => {
+
+    if (user) {
+      return res.status(400).json('errors')
+    } else {
+      const newUser = new User({
+        emailId: req.body.emailId,
+        password: req.body.password,
+        role: 'super_admin'
+      })
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+          newUser.save().then(user => {
+            res.json({success: true,user})
+          }).catch(err => {
+            console.log(err)
+            res.json({error: 'creating the user'})
+          })
+        })
+      })
+    }
+  })
+})
 router.post('/addMembers', passport.authenticate('diag_admin',{session: false}),(req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body)
   if (!isValid) {
@@ -113,60 +115,71 @@ router.post('/patientDetails',passport.authenticate('all_diag',{session: false})
   }
   console.log('here')
   console.log(req.body.patient, req.body.centre)
-  db.Patient.findOne({where:{mrNo:req.body.patient, centreCode:req.body.centre}}).then(patient => {
-    if(patient===null) {
+  request.get({api:API_KEY, qs: req.body},(err, res, body) => {
+    if(body.status==='FAIL') {
       console.log("here")
-      console.log({null:patient})
-      return res.json({patient: patient,invalid: true})
+      return res.json({patient: null,invalid: true})
     }
-    console.log({all:patient})
-    res.json({patient: patient,invalid: false})
-    //
-    // const newUpload = new Patient({
-    //   mrNo: req.body.patient,
-    //   uploadedBy: req.user.emailId
-    // })
-    // newUpload.save().then(pat => {
-    // }).catch(err => {
-    //   console.log(err)
-    //   res.json({patient: patient,invalid: true})
-    // })
-  }).catch(err => {
+    res.json({patient: body.patient_details,invalid: false})
+
+  })
+  // db.Patient.findOne({where:{mrNo:req.body.patient, centreCode:req.body.centre}}).then(patient => {
+  //   if(patient===null) {
+  //
+  //   }
+  //   console.log({all:patient})
+  //   //
+  //   // const newUpload = new Patient({
+  //   //   mrNo: req.body.patient,
+  //   //   uploadedBy: req.user.emailId
+  //   // })
+  //   // newUpload.save().then(pat => {
+  //   // }).catch(err => {
+  //   //   console.log(err)
+  //   //   res.json({patient: patient,invalid: true})
+  //   // })
+  // })
+    .catch(err => {
     res.status(400).json({inValid: 'Some thing is wrong try later'})
   })
 })
 router.post('/continueToUpload',passport.authenticate('all_diag', {session: false}),(req, res) => {
   console.log(req.user.id)
   User.findById(req.user.id).then(user => {
-    db.Patient.findOne({where:{mrNo:req.body.patient, centreCode:req.body.centre}}).then(patient => {
-      console.log({sqlPat:patient, dob: patient.dob})
-      let today = new Date();
-      let birthDate = new Date(patient.dob);
-      let age = today.getFullYear() - birthDate.getFullYear();
-      let m = today.getMonth() - birthDate.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-      }
-      console.log(birthDate, age)
-      const newUpload = new Patient({
-        mrNo: req.body.patient,
-        firstName: patient.firstName,
-        lastName: patient.lastName,
-        age: age,
-        gender: patient.gender,
-        uploadedBy: user.emailId,
-        diagCentreName: user.diagCentreName,
-        diagCentre: user.diagCentre,
-        centreShortCode: user.centreShortCode,
-        centreCode: user.centreCode
-      })
-      newUpload.save().then(pat => {
-        console.log({ "created user": pat._id })
-        res.json({ mid: pat._id })
-      }).catch(err => {
-        console.log(err)
-      })
-    })
+    // db.Patient.findOne({where:{mrNo:req.body.patient, centreCode:req.body.centre}}).then(patient => {
+      request.get({api:API_KEY, qs: req.body},(err, res, body) => {
+        if(body.status==='FAIL') {
+          console.log(err)
+        }
+        else {
+          console.log({ sqlPat: body.patient_details, dob: body.patient_details.dob })
+          let today = new Date();
+          let birthDate = new Date(body.patient_details.dob);
+          let age = today.getFullYear() - birthDate.getFullYear();
+          let m = today.getMonth() - birthDate.getMonth();
+          if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+          }
+          console.log(birthDate, age)
+          const newUpload = new Patient({
+            mrNo: req.body.patient,
+            firstName: body.patient_details.first_name,
+            lastName: body.patient_details.last_name,
+            age: age,
+            gender: body.patient_details.gender,
+            uploadedBy: user.emailId,
+            diagCentreName: user.diagCentreName,
+            diagCentre: user.diagCentre,
+            centreShortCode: user.centreShortCode,
+            centreCode: user.centreCode
+          })
+          newUpload.save().then(pat => {
+            console.log({ "created user": pat._id })
+            res.json({ mid: pat._id })
+          }).catch(err => {
+            console.log(err)
+          })
+        }
   })
 })
 router.post('/onDiscard',passport.authenticate('all_diag',{session: false}),(req, res) => {
