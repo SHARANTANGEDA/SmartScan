78d4812358770c3c8eb4ff9c2160c7aec22dfdcb
SHARANTANGEDA <saisarantangeda@gmail.com>
2019-07-01 08:02:41 +0530

    #Alpha Release version # Added File upload progress bar

--- client/src/components/upload/UploadFiles.js
+++ client/src/components/upload/UploadFiles.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import FileUpload from '../common/FileUpload'
 import classnames from 'classnames'
 import Select from 'react-select'
-import TextFieldGroup from '../common/TextFieldGroup'
 import TextAreaFieldGroup from '../common/TextAreaGroupField'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 class UploadFiles extends Component {
   constructor(props) {
@@ -15,7 +15,8 @@ class UploadFiles extends Component {
       spinner: false,
       category: null,
       remarks: '',
-      error: null
+      error: null,
+      progress:  0
     }
     // this.loadFiles = this.loadFiles.bind(this);
     this.onSelectType = this.onSelectType.bind(this)
@@ -43,6 +44,10 @@ class UploadFiles extends Component {
         }
       });
   }
+  myUploadProgress = () => (progress) => {
+    let percentage = Math.floor((progress.loaded * 100) / progress.total)
+    this.setState({progress:percentage})
+  }
   fileChanged(event) {
     this.setState({ [event.target.name]: event.target.files });
   }
@@ -67,7 +72,7 @@ class UploadFiles extends Component {
       data.append("file", file, file.name);
     });
 
-    axios.post('/api/upload/upload',data)
+    axios.post('/api/upload/upload',data,{onUploadProgress: this.myUploadProgress()})
       .then(res => {
         this.setState({spinner: false})
         if (res.data.success) {
@@ -87,7 +92,8 @@ class UploadFiles extends Component {
       {value: 'USG Abdomen', label: 'USG Abdomen'}, {value:'Blood Tests', label: 'Blood Tests'}]
     let spin = (
       <div>
-        <FileUpload/>
+        {/*<FileUpload/>*/}
+        <ProgressBar striped variant="info" now={this.state.progress} label={`${this.state.progress}%`}/>
         <p style={{ color: 'white', background: 'green' }} className='btn w-100'>
           You will be notified once upload is completed</p>
       </div>
